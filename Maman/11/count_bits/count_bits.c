7608126c73fc2215b497f7f5ece13a58e20e9333
hananson <45944206+hananson@users.noreply.github.com>
2020-11-10 23:02:11 +0200

    Update count_bits.c

--- Maman/11/count_bits/count_bits.c
+++ Maman/11/count_bits/count_bits.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+
+int count_bits(long int num);
+
 int main(void)
 {
-	printf("Hello World\n");    
+    long int input_number;
+    printf("Please insert a number:\n");
+    scanf("%ld", &input_number);
+    printf("%s%ld%s%d%s", "The number of bits in ", input_number, " is: ",count_bits(input_number),"\n");
 	return 0;
+}
+
+int count_bits(long int num)
+{
+    long int mask = 1L;
+    int counter = 0;
+    while(mask)
+    printf("%s%d%ld%s", "Counter is: ", counter, "\n Mask is: ", mask, "\n")
+    {
+        if(mask & num)
+            counter++;
+        mask<<1;
+    }
+    return counter;
 }
