510e12d01a2638f11c6cadc9335f5c1eea9eebe5
hananson <45944206+hananson@users.noreply.github.com>
2020-11-10 23:47:54 +0200

    Update count_bits.c

--- Maman/11/count_bits/count_bits.c
+++ Maman/11/count_bits/count_bits.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 
-int count_bits(long int num);
+int count_bits(long int num);  /*Prototype declaration*/
 
 int main(void)
 {
     long int input_number;
-    printf("Please insert a number:\n");
-    scanf("%ld", &input_number);
-    printf("%s%ld%s%d%s", "The number of bits in ", input_number, " is: ",count_bits(input_number),"\n");
+    printf("Please insert a decimal integer number:\n");  /* Get input number from user */
+    scanf("%ld", &input_number);  /* Store returned number in input_number */
+    printf("%s%ld%s%d%s", "The number of '1' bits in ", input_number, " is: ",count_bits(input_number),"\n");  /* Print results */
 	return 0;
 }
 
 int count_bits(long int num)
 {
-    long int mask = 1L;
-    int counter = 0;
-    while(mask)
-    printf("%s%d%ld%s", "Counter is: ", counter, "\n Mask is: ", mask, "\n")
+    long int mask = 1L;  /* Define mask variable in same sizeof num (...00001) */
+    short int counter = 0;  /* Initiate the returned value counter to 0. Unsigned Char (0-255) is enough here - long int number can have max 64 '1' bits */
+
+    while(mask) /* Make bitwise-and between mask and num, and shift mask left until mask is zero */
     {
-        if(mask & num)
-            counter++;
-        mask<<1;
+        if(mask & num)   /* Make bitwise-and between mask and num */
+            counter++;   /* In case mask & num is not zero: bit is on! -> increment counter by one */
+        mask = mask<<1;  /* Shift mask left once - to check the next bit. Shifting left to avoid cross platform/compiler right shift problems */
     }
     return counter;
 }
