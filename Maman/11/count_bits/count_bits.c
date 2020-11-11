d3af3e05430c0c81291e4e0ca1e8623c2c9251ea
hananson <45944206+hananson@users.noreply.github.com>
2020-11-11 23:14:26 +0200

    delivery

--- Maman/11/count_bits/count_bits.c
+++ Maman/11/count_bits/count_bits.c
@@ -1,26 +1,38 @@
+/* Count Bits - Hanan Sonnino, ID 301893905 
+
+This program gets a decimal number (long int) from the user - and prints the number of '1' bits in it. 
+The calculation is acheived using a while loop on the bit locations, masking them using bitwise-and 1. 
+
+Example: The input decimal number 15 in binary base is 1111, thus returned value will be 4 (bits in locations 1,2,3,4 are on). */
+
 #include <stdio.h>
 
 int count_bits(long int num);  /*Prototype declaration*/
 
 int main(void)
 {
-    long int input_number;
-    printf("Please insert a decimal integer number:\n");  /* Get input number from user */
+    long int input_number;  /* Declare input number that will be asked from user */
+    printf("Please insert a decimal integer number:\n");  /* Ask for an input number from user */
     scanf("%ld", &input_number);  /* Store returned number in input_number */
     printf("%s%ld%s%d%s", "The number of '1' bits in ", input_number, " is: ",count_bits(input_number),"\n");  /* Print results */
 	return 0;
 }
 
-int count_bits(long int num)
+
+/* count_bits
+Purpose: Count the number of '1' bits in a decimal number
+Args: num (long int int) - decimal input number
+Returns: counter (short int) - number of '1' bits. */
+int count_bits(long int num)  
 {
     long int mask = 1L;  /* Define mask variable in same sizeof num (...00001) */
-    short int counter = 0;  /* Initiate the returned value counter to 0. Unsigned Char (0-255) is enough here - long int number can have max 64 '1' bits */
+    short int counter = 0;  /* Initiate the returned value counter to 0 */
 
-    while(mask) /* Make bitwise-and between mask and num, and shift mask left until mask is zero */
+    while(mask) /* Make bitwise-and between mask and num, and shift mask left once until mask is zero */
     {
         if(mask & num)   /* Make bitwise-and between mask and num */
             counter++;   /* In case mask & num is not zero: bit is on! -> increment counter by one */
         mask = mask<<1;  /* Shift mask left once - to check the next bit. Shifting left to avoid cross platform/compiler right shift problems */
     }
-    return counter;
+    return counter;  /* Return counted odd bits */
 }
--- Maman/11/count_odd_bits/count_odd_bits.c
+++ Maman/11/count_odd_bits/count_odd_bits.c
@@ -1,8 +1,15 @@
-/* Count Odd Bits - Hanan Sonnino, ID 301893905 */
+/* Count Odd Bits - Hanan Sonnino, ID 301893905 
+
+This program gets decimal numbers (unsigned short) from the user - and prints the number of odd '1' bits. 
+The program continues getting more decimal inputs from the user - until EOF is received.
+The calculation is acheived using a while loop on the odd bit locations, masking them using bitwise-and 1. 
+
+Example: The input decimal number 15 in binary base is 1111, thus returned value will be 2 (odd bits in locations 1, 3 are on). */
+
 
 #include <stdio.h>  
 
-int count_odd_bits(unsigned short int num);  /*Prototype declaration*/
+int count_odd_bits(unsigned short int num);  /* Prototype declaration */
 
 int main(void)
 {
@@ -13,12 +20,16 @@ int main(void)
 	return 0;
 }
 
-int count_odd_bits(unsigned short int num) /* Count the number of '1' odd-bits in a decimal number */
+/* count_odd_bits
+Purpose: Count the number of '1' odd-bits in a decimal number 
+Args: num (unsigned short int) - decimal input number
+Returns: counter (unsigned short int) - number of odd '1' bits. */
+int count_odd_bits(unsigned short int num) 
 {
     unsigned short int mask = 2;  /* Define mask variable in same sizeof num (...000010) */
     unsigned short int counter = 0;  /* Initiate the returned value counter to 0 */
 
-    while(mask) /* Make bitwise-and between mask and num, and shift mask left until mask is zero */
+    while(mask) /* Make bitwise-and between mask and num, and shift mask left twice until mask is zero */
     {
         if(mask & num)   /* Make bitwise-and between mask and num */
             counter++;   /* In case mask & num is not zero: bit is on! -> increment counter by one */
