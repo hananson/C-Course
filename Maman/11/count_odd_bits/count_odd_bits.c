c07d5ea46598d4917734ea5d96a22ae65035c3c9
hananson <45944206+hananson@users.noreply.github.com>
2020-11-11 22:36:06 +0200

    Update count_odd_bits.c

--- Maman/11/count_odd_bits/count_odd_bits.c
+++ Maman/11/count_odd_bits/count_odd_bits.c
@@ -1,17 +1,19 @@
-#include <stdio.h>
+/* Count Odd Bits - Hanan Sonnino, ID 301893905 */
+
+#include <stdio.h>  
 
 int count_odd_bits(unsigned short int num);  /*Prototype declaration*/
 
 int main(void)
 {
-    long int input_number;
-    printf("Please insert a decimal integer number:\n");  /* Get input number from user */
-    scanf("%ld", &input_number);  /* Store returned number in input_number */
-    printf("%s%ld%s%d%s", "The number of '1' odd bits in ", input_number, " is: ",count_odd_bits(input_number),"\n");  /* Print results */
+    unsigned short int input_number;  /* Declare input number that will be asked from user */
+    printf("Please insert a decimal integer number:\n");  /* Ask for an input number from user */
+    while (scanf("%hu", &input_number) != EOF) /* Store returned number in input_number, if EOF: break */
+        printf("%s%d%s%hu%s", "The number of '1' odd bits in ", input_number, " is: ",count_odd_bits(input_number),"\n");  /* Print results */
 	return 0;
 }
 
-int count_odd_bits(unsigned short int num)
+int count_odd_bits(unsigned short int num) /* Count the number of '1' odd-bits in a decimal number */
 {
     unsigned short int mask = 2;  /* Define mask variable in same sizeof num (...000010) */
     unsigned short int counter = 0;  /* Initiate the returned value counter to 0 */
@@ -22,5 +24,5 @@ int count_odd_bits(unsigned short int num)
             counter++;   /* In case mask & num is not zero: bit is on! -> increment counter by one */
         mask = mask<<2;  /* Shift mask left twice - to check the next odd bit. Shifting left to avoid cross platform/compiler right shift problems */
     }
-    return counter;
+    return counter;  /* Return counted odd bits */
 }
