8095d9eee6994d3dd73811e28cf4add4a0a9a43a
hananson <45944206+hananson@users.noreply.github.com>
2020-12-19 22:50:50 +0200

    works

--- Maman/22/mat.h
+++ Maman/22/mat.h
@@ -1,7 +1,8 @@
 #ifndef MAT_H
 #define MAT_H
+#include <stdio.h>
 
 typedef int mat[4][4];
-void printIntArray(int *array, int array_len, char *msg)
+void printIntArray(int *array, int array_len, char *msg);
 
 #endif
--- Maman/22/mymat.c
+++ Maman/22/mymat.c
@@ -1,11 +1,8 @@
-#include <stdio.h>
 #include "mat.h"
-
 
 
 int main(void)
 {
-    /*mat MAT_A, MAT_B, MAT_C, MAT_D, MAT_E, MAT_F = {{0},{0}};*/
     int a=5;
     printf("%d", a);
     return 0;
