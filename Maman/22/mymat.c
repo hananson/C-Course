ebee9263c576a154c7640989cc4c3a2e32a5d502
hananson <45944206+hananson@users.noreply.github.com>
2020-12-19 23:24:03 +0200

    Update mymat.c

--- Maman/22/mymat.c
+++ Maman/22/mymat.c
@@ -3,10 +3,12 @@
 
 int main(void)
 {
-    mat MAT_A = EMPTY_MAT;
+    mat MAT_A, MAT_B, MAT_C, MAT_D, MAT_E, MAT_F = EMPTY_MAT;
     /*print4x4IntArray(MAT_A);*/
 
     int i, j;
+    char *input;
+
     for(i=0;i<4;i++)
     {
         for(j=0;j<4;j++)
@@ -14,6 +16,11 @@ int main(void)
         printf("\n");
     }
     printf("\n");
+
+    printf("Please Enter the desired command:\n");
+    
+    scanf("%c", &input);
+    printf("%c", input);
 
     return 0;
 }
