d46c47753528be322dfd458ca21395f67573354a
hananson <45944206+hananson@users.noreply.github.com>
2020-12-19 23:06:23 +0200

    Update mymat.c

--- Maman/22/mymat.c
+++ Maman/22/mymat.c
@@ -5,6 +5,16 @@ int main(void)
 {
     mat MAT_A = EMPTY_MAT;
     print4x4IntArray(MAT_A);
+
+    int i, j;
+    for(i=0;i<4;i++)
+    {
+        for(j=0;j<4;j++)
+            printf("%d ", MAT_A[i][j]);
+        printf("\n");
+    }
+    printf("\n");
+
     return 0;
 }
 
